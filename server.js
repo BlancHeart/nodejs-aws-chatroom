360204e4241f0af51ae72fe9ca682e716c72cf5f
CyberCyclist <QuantizedThomas@gmail.com>
2015-04-01 20:05:01 -0700

    using chatter module
    chatter module is from lynda.
    Reference:
    http://wwwlyndacom.proxy.lib.sfu.ca/Nodejs-tutorials/Reusing-chatrooms/101554/110770-4.html

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 var io = require('socket.io'),
-  connect = require('connect');
+  connect = require('connect'),
+  chatter = require('chatter');
 
-var app = connect().use(connect.static('public')).listen(3000);
-var socket = io.listen(app);
+var app = connect().use(connect.static('public')).listen(8080);
+var chat_room = io.listen(app);
+
+chatter.set_sockets(chat_room.sockets);
+
+chat_room.sockets.on('connection', function (socket) {
+  chatter.connect_chatter({
+    socket: socket,
+    username: socket.id
+  });
+});
